16fba9e6d6fa233f8de135158a48841d52b56b4d
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-04 18:41:13 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,22 @@
-
 import Player from '@vimeo/player';
 
-
 const player = new Player('vimeo-player', {
-    id: 19231868,
-    width: 640
+  id: 19231868,
+  width: 640,
 });
 
-player.on('play', function() {
-    console.log('played the video!');
+player.on('play', function () {
+  console.log('played the video!');
 });
+
+player.on('timeupdate', onTimeUpdate);
+
+function onTimeUpdate({
+  }) {
+
+  localStorage.setItem('videoplayer-current-time', JSON.stringify({
+    duration: 61.857,
+    percent: 0.049,
+    seconds: 3.034,
+  }));
+}
