f9daa7a7280292e42c21797182e5b82877c3abf6
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-04 16:55:27 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,9 @@
 
 import Player from '@vimeo/player';
 
-const player = new Player('handstick', {
-    id: 'vimeo-player',
+
+const player = new Player('vimeo-player', {
+    id: 19231868,
     width: 640
 });
 
