164fc57b756b56456929bada812a9d371920fef4
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-04 16:36:29 +0300

    add player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
 
+import Player from '@vimeo/player';
+
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
