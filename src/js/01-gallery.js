9224fd2a3b76114b011f87ceae6b788eaf31b90b
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-04 12:57:53 +0300

    01-lihtbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -40,22 +40,22 @@ lightbox.on("closed.simplelightbox", function () {
 });
 
 
-// const lazyImages = refs.imageContainer.querySelectorAll(".gallery__image");
+const lazyImages = refs.imageContainer.querySelectorAll(".gallery__image");
 
-// lazyImages.forEach((image) =>
-//   image.addEventListener("load", onImageLoaded, { once: true })
-// );
+lazyImages.forEach((image) =>
+  image.addEventListener("load", onImageLoaded, { once: true })
+);
 
-// function onImageLoaded(event) {
-//   event.target.classList.add("appear");
-// }
+function onImageLoaded(event) {
+  event.target.classList.add("appear");
+}
 
-// lazyImages.forEach((image) =>
-//   image.addEventListener("mouseenter", onMouseEnter)
-// );
+lazyImages.forEach((image) =>
+  image.addEventListener("mouseenter", onMouseEnter)
+);
 
 
-// function onMouseEnter(event) {
-//   event.target.style.transitionDelay = '100ms';
-//   event.target.style.transitionDuration = "500ms";
-// }
+function onMouseEnter(event) {
+  event.target.style.transitionDelay = '100ms';
+  event.target.style.transitionDuration = "500ms";
+}
