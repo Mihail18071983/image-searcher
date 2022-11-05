5cba442dc3e5c4a73199706a29bf5fd1dd9ac0d1
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-05 20:24:34 +0200

    update 02.gallery

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,18 @@
 import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
 const ifFrameRef = document.querySelector('#vimeo-player');
 const player = new Player(ifFrameRef);
 
-
-player.on('timeupdate', onTimeUpdate);
+player.on('timeupdate', throttle(onTimeUpdate,1000));
 
 function onTimeUpdate(e) {
   e.preventDefault();
   localStorage.setItem('videoplayer-current-time', JSON.stringify(e));
-  
 }
 
-const getContentOfLocalStorage = JSON.parse(localStorage.getItem('videoplayer-current-time'));
- player.setCurrentTime(getContentOfLocalStorage.seconds)
-
+const getContentOfLocalStorage = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+player.setCurrentTime(getContentOfLocalStorage.seconds);
