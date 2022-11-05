07c5b41fc4e8d37c062b4720f175bd8e2b6e90a2
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-05 19:39:26 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,33 +12,20 @@ const player = new Player('vimeo-player', {
 player.on('timeupdate', onTimeUpdate);
 
 function onTimeUpdate(e) {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(e));
   e.preventDefault();
-    console.log(e);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(e));
+  
+    // console.log(e);
 }
 
 const getContentOfLocalStorage = JSON.parse(localStorage.getItem('videoplayer-current-time'));
-console.log(getContentOfLocalStorage);
+// console.log(getContentOfLocalStorage);
 
-sessionStorage.setItem("is_reloaded", true);
+// sessionStorage.setItem("is_reloaded", true);
 
-if (sessionStorage.getItem("is_reloaded")) {
-  console.log('Страница перезагружена'); 
-}
+// if (sessionStorage.getItem("is_reloaded")) {
+//   console.log('Страница перезагружена'); 
+// }
 
- player.setCurrentTime(getContentOfLocalStorage.seconds).then(function(seconds) {
-   seconds = getContentOfLocalStorage.seconds;
-   console.log(seconds);
-}).catch(function(error) {
-    switch (error.name) {
-      case 'RangeError':
-        (seconds<0 || seconds>getContentOfLocalStorage.duration)
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+ player.setCurrentTime(getContentOfLocalStorage.seconds)
 
