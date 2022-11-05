033d83707dbece36eaef14d2eae6ff19bb778ac0
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-05 11:09:26 +0300

    update 02-gallery

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -16,4 +16,21 @@ function onTimeUpdate(e) {
     console.log(e);
 }
 
+if (sessionStorage.getItem("is_reloaded")) {
+	console.log('Страница перезагружена')
+}
+
+// player.setCurrentTime(30.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
 
