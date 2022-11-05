04cc48305417d39e22f48a4df515cf7b108e200a
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-05 20:34:12 +0200

    02-gallery

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,7 +8,6 @@ const player = new Player(ifFrameRef);
 player.on('timeupdate', throttle(onTimeUpdate,1000));
 
 function onTimeUpdate(e) {
-  e.preventDefault();
   console.log(e);
   localStorage.setItem('videoplayer-current-time', JSON.stringify(e));
 }
