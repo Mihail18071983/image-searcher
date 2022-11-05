f29cfc69bfcd6818e8edb54c0747079cc55e7e16
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-05 19:55:53 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,10 +2,7 @@ import Player from '@vimeo/player';
 
 const ifFrameRef = document.querySelector('#vimeo-player');
 const player = new Player(ifFrameRef);
-// const player = new Player('vimeo-player', {
-//   id: 19231868,
-//   width: 640,
-// });
+
 
 player.on('timeupdate', onTimeUpdate);
 
@@ -13,17 +10,8 @@ function onTimeUpdate(e) {
   e.preventDefault();
   localStorage.setItem('videoplayer-current-time', JSON.stringify(e));
   
-    // console.log(e);
 }
 
 const getContentOfLocalStorage = JSON.parse(localStorage.getItem('videoplayer-current-time'));
-// console.log(getContentOfLocalStorage);
-
-// sessionStorage.setItem("is_reloaded", true);
-
-// if (sessionStorage.getItem("is_reloaded")) {
-//   console.log('Страница перезагружена'); 
-// }
-
  player.setCurrentTime(getContentOfLocalStorage.seconds)
 
