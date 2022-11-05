32b90116b519c0392c9242842c0bca40d0535c4d
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-05 19:47:35 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,10 @@
 import Player from '@vimeo/player';
 
-const player = new Player('vimeo-player', {
-  id: 19231868,
-  width: 640,
-});
-
-// player.on('play', function () {
-//   console.log('played the video!');
+ifFrameRef = document.querySelector('#vimeo-player');
+const player = new Player(ifFrameRef);
+// const player = new Player('vimeo-player', {
+//   id: 19231868,
+//   width: 640,
 // });
 
 player.on('timeupdate', onTimeUpdate);
