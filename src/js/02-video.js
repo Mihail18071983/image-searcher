aa546e1a7b0fda23e276b9076914ec8f6ce0c93e
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-06 14:36:24 +0200

    update 02.jallery

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,14 +5,20 @@ const throttle = require('lodash.throttle');
 const ifFrameRef = document.querySelector('#vimeo-player');
 const player = new Player(ifFrameRef);
 
-player.on('timeupdate', throttle(onTimeUpdate,5000));
+player.on('timeupdate', throttle(onTimeUpdate,1000));
 
 function onTimeUpdate(e) {
-  console.log(e);
   localStorage.setItem('videoplayer-current-time', JSON.stringify(e));
 }
 
 const getContentOfLocalStorage = JSON.parse(
   localStorage.getItem('videoplayer-current-time')
 );
+
 player.setCurrentTime(getContentOfLocalStorage.seconds);
+
+
+
+window.addEventListener('touchstart', function(e) {
+  e.preventDefault();
+}, {passive:false}); // <-- mark the event listerner as NOT passive
