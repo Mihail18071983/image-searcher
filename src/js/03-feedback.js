a75d95418649a0aa28c02dcbd9f596ea0d444373
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-06 20:12:51 +0200

    03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,22 @@
 const refs = {
-    form: document.querySelector('.feedback-form'),
-}
+  form: document.querySelector('.feedback-form'),
+};
 
-const LOCALSTORAGE_KEY = "feedback-form-state";
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 refs.form.addEventListener('input', onInputForm);
 
 function onInputForm(e) {
-    e.preventDefault();
-    console.log(e.target.name);
-   
-
-    // localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(elements));
-
+  e.preventDefault();
+  const message = refs.form.elements.message.value;
+  const email = refs.form.elements.email.value;
+  //   console.log('e.target', e.target);
+  //   console.log('e.currentTarget', e.currentTarget);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({ message, email }));
 }
+
+function updateOutput() {
+    const outputTextcontent = localStorage.getItem(LOCALSTORAGE_KEY) || '';
+  return outputTextcontent;
+}
+console.log (updateOutput())
