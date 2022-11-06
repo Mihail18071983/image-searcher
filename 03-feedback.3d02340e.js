7299011a5f6f5e4d01dbcbfbd90f69ee3946138d
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-06 15:39:19 +0000

    Deploying to gh-pages from @ Mihail18071983/goit-js-hw-08@5e58ababbc8949629a7735fb5a028e83785f2dba 🚀

---
+++ 03-feedback.3d02340e.js
@@ -0,0 +1,2 @@
+({form:document.querySelector(".feedback-form")}).form.addEventListener("input",(function(e){e.preventDefault(),console.log(e.target.name)}));
+//# sourceMappingURL=03-feedback.3d02340e.js.map
---
+++ 03-feedback.d2991c45.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(function(e){e.preventDefault(),console.log(e.target.name)}));
+//# sourceMappingURL=03-feedback.d2991c45.js.map
