845bb760074ae93a017d37d0176c6d39b28b84e4
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-07 10:35:44 +0000

    Deploying to gh-pages from @ Mihail18071983/goit-js-hw-08@89153c51ebdf88efc2f1a103286e662cf395eb8a 🚀

--- 03-feedback.9448c058.js
+++
@@ -1,2 +0,0 @@
-!function(){const e={form:document.querySelector(".feedback-form")},t="feedback-form-state";e.form.addEventListener("input",(function(o){o.preventDefault();const a=e.form.elements.message.value,m=e.form.elements.email.value;localStorage.setItem(t,JSON.stringify({message:a,email:m}))})),console.log(localStorage.getItem(t)||"")}();
-//# sourceMappingURL=03-feedback.9448c058.js.map
