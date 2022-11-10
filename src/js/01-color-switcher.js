103b28b9af06361ae39d2c1e7e0b2090b167ad8c
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-10 17:42:42 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.body,
+};
+let intervalID = null;
 
+refs.btnStart.addEventListener('click', startChangeBodyColor);
+refs.btnStop.addEventListener('click', stopChangeBodyColor);
+refs.btnStop.disabled = true;
+
+function startChangeBodyColor(e) {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+  intervalID = setInterval(setBodyColor, 1000);
+}
+
+function stopChangeBodyColor(e) {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+  clearInterval(intervalID);
+  intervalID = 0;
+}
+
+function setBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
