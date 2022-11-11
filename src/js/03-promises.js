d45da7414179acacc246a6d95526ed7bbea5ddb7
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-11 20:38:30 +0200

    03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+const refs = {
+  form: document.querySelector('form'),
+  sumbmitBtn: document.querySelector('button'),
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(console.log(`✅ Fulfilled promise ${position} in ${delay}ms`));
+      } else {
+        reject(console.log(`❌ Rejected promise ${position} in ${delay}ms`));
+      }
+    }, delay)
+  );
+  // const shouldResolve = Math.random() > 0.3;
+  // if (shouldResolve) {
+  //   // Fulfill
+  // } else {
+  //   // Reject
+  // }
+}
+
+createPromise()
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+refs.form.addEventListener('submit', onCreatePromiseStart);
+
+function onCreatePromiseStart(e) {
+  e.preventDefault();
+  const firstDelay = refs.form.elements.delay.value;
+  setTimeout(createPromise, firstDelay);
 }
