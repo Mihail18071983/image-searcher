e46cb1915f9c05a04d2783a95d084c211a40d1e6
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-21 16:06:52 +0200

    02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,5 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-// import { options } from './options';
 import { convertMs } from './convertMs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
