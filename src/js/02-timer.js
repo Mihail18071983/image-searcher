2d6bf1888b3bea26b809886866b4a03d737ac5b3
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-22 11:52:35 +0200

    02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,7 +3,6 @@ import 'flatpickr/dist/flatpickr.min.css';
 import { convertMs } from './convertMs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
 let timerID = null;
 let userDate = null;
 
@@ -14,7 +13,7 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     selectedDates[0] <= options.defaultDate
-      ? (Notify.failure('Please choose a date in the future'),
+      ? (Notify.info('Please choose a date in the future'),
         (refs.button.disabled = true))
       : (refs.button.disabled = false);
     userDate = selectedDates[0];
@@ -34,9 +33,8 @@ export const refs = {
 
 window.addEventListener('click', startTimer);
 
-function startTimer(e) { 
-  if (e.target.nodeName !== "BUTTON") return;
-  Notify.success("The countdown has started");
+function startTimer(e) {
+  if (e.target.nodeName !== 'BUTTON') return;
   timerID = setInterval(countDownTimer, 1000);
   refs.button.disabled = true;
   refs.input.disabled = true;
@@ -45,26 +43,30 @@ function startTimer(e) {
 function countDownTimer() {
   userDate = Date.parse(refs.input.value);
   const diff = userDate - Date.now();
-  let {days, hours, minutes, seconds} = getTimeComponents(diff) ;
+  let { days, hours, minutes, seconds } = getTimeComponents(diff);
+  if (userDate <= Date.now()) {
+    Notify.info('Please, choose date in future');
+    clearInterval(timerID);
+    refs.input.disabled = false;
+  }
   if (diff <= 1000) {
     clearInterval(timerID);
     seconds = getTimeComponents(0).seconds;
     minutes = getTimeComponents(0).minutes;
     hours = getTimeComponents(0).hours;
     days = getTimeComponents(0).days;
-    
+    refs.input.disabled = false;
   }
-  updateCountDownUI({ seconds, minutes, hours, days }); 
+  updateCountDownUI({ seconds, minutes, hours, days });
 }
 
 function getTimeComponents(time) {
-  return convertMs(time)
+  return convertMs(time);
 }
 
 function updateCountDownUI({ seconds, minutes, hours, days }) {
   refs.secondsUi.textContent = seconds;
   refs.minutesUi.textContent = minutes;
   refs.hoursUi.textContent = hours;
-  refs.daysUi.textContent = days;  
-  
+  refs.daysUi.textContent = days;
 }
