bcee8404cd6208fa5092f423b3c61f56cb8458c3
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-11-27 16:48:40 +0000

    Deploying to gh-pages from @ Mihail18071983/goit-js-hw-10@4d09c582fc03484ce3504482f06a808e68dd5288 🚀

--- 01-color-switcher.dfdfad0b.js
+++
@@ -1,2 +0,0 @@
-const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.body};let n=null;function e(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.btnStart.addEventListener("click",(function(o){t.btnStart.disabled=!0,t.btnStop.disabled=!1,n=setInterval(e,1e3)})),t.btnStop.addEventListener("click",(function(e){t.btnStart.disabled=!1,t.btnStop.disabled=!0,clearInterval(n),n=0})),t.btnStop.disabled=!0;
-//# sourceMappingURL=01-color-switcher.dfdfad0b.js.map
