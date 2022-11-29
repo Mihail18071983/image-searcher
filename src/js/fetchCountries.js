dd31b7c6a97fbdc1fe70d8b93307bcff710ca033
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2022-11-29 18:07:22 +0200

    update

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,10 @@
+import axios from 'axios';
 import { Notify } from 'notiflix';
 
 export function fetchCountries(name) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then((response) => {
-      if (!response.ok) {
-        Notify.failure('Oops, there is no country with that name');
-      }
-      return response.json();
-    }
-  ).catch(err=>console.log(err))
+  return axios
+    .get(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    )
+    .then(({ data }) => data);
 }
