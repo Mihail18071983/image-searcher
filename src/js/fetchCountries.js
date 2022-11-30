2373b7cd497de1edfd6118f967818726988492a7
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2022-11-30 19:21:10 +0200

    start

--- src/js/fetchCountries.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function fetchCountries(name) {
-  return axios
-    .get(
-      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-    )
-    .then(({ data }) => data);
-}
